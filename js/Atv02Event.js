00d84c8b830a5c015638852cf4c6e1012f1d8269
CaioBattisti <cluizbattisti@gmail.com>
2025-07-03 20:27:09 -0300

    save28

--- js/Atv02Event.js
+++ js/Atv02Event.js
@@ -9,9 +9,9 @@ function mouseOut()
 
 function mouseOver2()
 {
-    document.imagem.src="";
+    document.imagem.src="image/Atv02_jojoBizarreAdventure.jpg";
 }
 function mouseOut2()
 {
-    document.imagem.src="";
+    document.imagem.src="image/Atv02_AttackTitan.jpg";
 }
