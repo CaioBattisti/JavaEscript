cbbb281489e31dd735f62289353b34c38ecdd661
CaioBattisti <cluizbattisti@gmail.com>
2025-07-03 19:07:55 -0300

    save25

---
+++ js/Atv02Event.js
@@ -0,0 +1,16 @@
+function mouseOver()
+{
+    document.imagem.src="image/Atv02_Naruto.jpg";
+}
+function mouseOut()
+{
+    document.imagem.src="image/Atv02_OnePiece.jpg";
+}
+function mouseOver2()
+{
+    document.imagem.src="";
+}
+function mouseOut2()
+{
+    document.imagem.src="";
+}
