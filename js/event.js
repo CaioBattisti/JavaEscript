f99e2248bc7e200dc9fe379b65ffdd87787a2ff6
CaioBattisti <cluizbattisti@gmail.com>
2025-07-02 22:03:45 -0300

    save21

--- js/event.js
+++ js/event.js
@@ -2,9 +2,9 @@
 
 function mouseOver()
 {
-    document.imagem.src="../image/cat02.jpg";
+    document.imagem.src="image/cat02.jpg";
 }
 function mouseOut()
 {
-    document.imagem.src="../image/cat01.jpg";
+    document.imagem.src="image/cat01.jpg";
 }
